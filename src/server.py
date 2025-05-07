afc8e0c5f98790df5b726fc0adb41081270a8b07
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-05-08 00:05:19 +0200

    first version of server.py

---
+++ src/server.py
@@ -0,0 +1,20 @@
+import socket
+
+
+class HTTPServer:
+    pass
+
+
+class HTTPRequestHandler:
+    pass
+
+
+class Server(HTTPRequestHandler):
+    pass
+
+
+hostname = "localhost"
+port = "8080"
+
+if __name__ == "__main__":
+    pass
